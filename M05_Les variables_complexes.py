50bb4fa6bada69d07d9983bec93a965f42329acc
zohera El Fakhadi <zohera.elfakhadi@gmail.com>
2026-06-23 14:50:04 +0200

    Ajout exemples manipulation chaînes (center, rjust, zfill, strip)

--- M05_Les variables_complexes.py
+++ M05_Les variables_complexes.py
@@ -40,3 +40,27 @@ l2[0].append('d')
 # Affichage
 print(l2)  # la copie est modifiée
 print(l)   # l originale reste intacte
+
+
+# Chaîne de caractères
+s = 'test'
+
+# Centre la chaîne sur une largeur de 30 caractères
+# Des espaces sont ajoutés à gauche et à droite
+print(s.center(30))
+
+# Aligne la chaîne à droite sur une largeur de 30 caractères
+# Des espaces sont ajoutés à gauche
+print(s.rjust(30))
+
+# Complète la chaîne à gauche avec des zéros
+# jusqu'à obtenir une longueur totale de 30 caractères
+print(s.zfill(30))
+
+
+# Ajoute des zéros à gauche jusqu'à obtenir une longueur de 4 caractères
+print('42'.zfill(4))
+
+# Supprime au début et à la fin les caractères :
+# espace, tiret (*) et astérisque (*)
+print(' -* test -* '.strip(' -*'))
